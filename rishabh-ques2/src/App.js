af2b290068877875b6e4c7c12fb96654c98745b1
Anshul Singh <95980982+whyanshul@users.noreply.github.com>
2022-07-07 02:37:13 +0530

    Update App.js

--- rishabh-ques2/src/App.js
+++ rishabh-ques2/src/App.js
@@ -1,15 +1,26 @@
-import {BrowserRouter,Router,Route} from 'react-router-dom';
+import logo from './logo.svg';
 import './App.css';
-import React from 'react';
-import HomePage from './src/Pages/HomePage/HomePage.component';
-import Contactus from './src/Pages/ContactUsPage/ContactUsPage.component';
-
+import React ,{useState}from 'react'
 function App() {
-  
+const [data,setData]=useState(null)
+const [print,setPrint]=useState(false)
 
+  function getData(val)
+  {
+    console.warn(val.target.value)
+    setData(val.target.value)
+    setPrint(false)
+  }
   return (
-      
-   
+    <div className="App">
+     {
+       print?
+       <h1> {data}</h1>
+       :null
+     }
+    <input type="text" onChange={getData} />
+    <button onClick={()=>setPrint(true)} >Print Data</button>
+    </div>
   );
 }
 
